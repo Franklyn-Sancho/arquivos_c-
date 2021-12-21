f8f1e1f20f38b2c86f7e252d59a9c38e0be31b1a
franklyn-sancho <franklyn.hs@gmail.com>
2021-12-21 12:32:57 -0300

    principios de array

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # arquivos_c-
 
 #vetores e arrays. 
-#página 65. 
+#página 65. OK.
+#página 72. PENDENTE.
+
 
---
+++ teste.cpp
@@ -0,0 +1,16 @@
+//programa para testar array. 
+
+#include <iostream>
+using namespace std;
+
+#define TAM 10
+
+int main() {
+
+    int ops[TAM], i;
+
+    for(i = 0; i <= TAM; i += 1)
+    {
+        ops[i] = 0;
+    }
+}
